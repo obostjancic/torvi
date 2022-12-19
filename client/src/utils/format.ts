2c14b3744180fb5f240d9f1e11db3ee9f5aeb27b
Ogi <86684834+obostjancic@users.noreply.github.com>
2022-12-19 12:01:21 +0100

    build fix

--- client/src/utils/format.ts
+++ client/src/utils/format.ts
@@ -2,4 +2,4 @@ import { format } from 'date-fns';
 
 export const formatDate = (date: Date): string => format(date, 'dd.MM.yyyy');
 
-export const formatJSON = (json: Record<string, unknown>): string => JSON.stringify(json, null, 2);
+export const formatJSON = (json: Record<string, any>): string => JSON.stringify(json, null, 2);
