c3811786af031d8a1d9f8e623d29aa14fc9c481c
Ogi <86684834+obostjancic@users.noreply.github.com>
2022-12-19 12:50:13 +0100

    added api url env var

--- client/src/model/search.ts
+++ client/src/model/search.ts
@@ -21,7 +21,7 @@ export class SearchApi {
 
   constructor() {
     this.instance = axios.create({
-      baseURL: 'http://localhost:8080/api',
+      baseURL: `${import.meta.env.VITE_API_URL}/api`,
     });
   }
 
